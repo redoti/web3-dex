1b36b5483dc185bbcd70c4e88aab5496299c89bb
redoti <zulfikarredoti@gmail.com>
2023-06-21 02:57:51 +0700

    add balance on portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,245 +1,139 @@
 import React, { useState, useEffect } from "react";
-import { Table, Avatar, Button, Space, Modal, Form, Input, Select,message } from "antd";
+import { Table, Avatar } from "antd";
 import axios from "axios";
 import { useAccount } from "wagmi";
-import data from "../tokenList.json";
+import tokenList from "../tokenList.json";
 import "../Portfolio.css";
-
-const { Option } = Select;
+import { Alchemy, Network } from "alchemy-sdk";
 
 function Portfolio() {
-  const [tableData, setTableData] = useState([]);
-  const [modalVisible, setModalVisible] = useState(false);
-  const [editedHolding, setEditedHolding] = useState("");
-  const [form] = Form.useForm();
-  const [editIndex, setEditIndex] = useState(-1);
-  const [addModalVisible, setAddModalVisible] = useState(false);
-  const [addFormValues, setAddFormValues] = useState({});
+  const { isConnected, address } = useAccount();
+  const [tokenPrices, setTokenPrices] = useState({});
+  const [balances, setBalances] = useState([]);
+
+  const config = {
+    apiKey: "PuCUG-mzhjx1W_n6WB0VRDNd_Rr0-TfC",
+    network: Network.ARB_MAINNET,
+  };
+  const alchemy = new Alchemy(config);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const tokenData = await Promise.all(
-        data.map(async (coin) => {
-          const price = await fetchTokenPrice(`arbitrum:${coin.address}`);
-          return {
-            ...coin,
-            price: price || 1,
-            holding: 1,
-          };
-        })
-      );
+    const fetchTokenPrices = async () => {
+      try {
+        const tokenPriceData = {};
 
-      setTableData(tokenData);
+        await Promise.all(
+          tokenList.map(async (token) => {
+            const response = await axios.get(`https://coins.llama.fi/prices/current/arbitrum:${token.address}`);
+            const tokenPrice = response?.data?.coins?.[`arbitrum:${token.address}`]?.price;
+
+            if (tokenPrice !== undefined) {
+              tokenPriceData[`arbitrum:${token.address}`] = tokenPrice;
+            } else {
+              console.error(`Invalid response for token address: arbitrum:${token.address}`);
+            }
+          })
+        );
+
+        setTokenPrices(tokenPriceData);
+      } catch (error) {
+        console.error('Error fetching token prices:', error);
+      }
     };
 
-    fetchData();
+    fetchTokenPrices();
   }, []);
 
-  async function fetchTokenPrice(tokenAddress) {
-    const apiUrl = `https://coins.llama.fi/prices/current/${tokenAddress}`;
+  useEffect(() => {
+    const fetchTokenBalances = async () => {
+      try {
+        const tokenContractAddresses = tokenList.map((token) => token.address);
+        const data = await alchemy.core.getTokenBalances(address, tokenContractAddresses);
 
-    try {
-      const response = await axios.get(apiUrl);
-      const data = response.data;
-      const price = data.coins[tokenAddress].price;
+        const tokenBalances = await Promise.all(
+          tokenContractAddresses.map(async (tokenAddress, index) => {
+            const metadata = await alchemy.core.getTokenMetadata(tokenAddress);
+            const tokenName = metadata.name + "(" + metadata.symbol + ")";
+            const tokenBalance = data["tokenBalances"][index]["tokenBalance"] / Math.pow(10, metadata.decimals);
+            const roundedBalance = tokenBalance.toFixed(3);
 
-      return price;
-    } catch (error) {
-      console.error("Error fetching token price:", error);
-      return null;
+
+            return {
+              name: tokenName,
+              balance: roundedBalance,
+              decimals: metadata.decimals,
+              address: tokenAddress,
+              img: metadata.logo,
+              ticker: metadata.symbol,
+            };
+          })
+        );
+
+        setBalances(tokenBalances);
+      } catch (error) {
+        console.error("Error fetching token balances:", error);
+      }
+    };
+
+    if (isConnected) {
+      fetchTokenBalances();
     }
-  }
-
-  const { isConnected } = useAccount();
+  }, [isConnected, address]);
 
   const columns = [
     {
       title: "Icon",
       dataIndex: "img",
       key: "img",
-      render: (text, token) => <Avatar src={token.img} />,
+      render: (_, token) => <Avatar src={token.img} />,
     },
     {
       title: "Token Name",
       dataIndex: "name",
       key: "name",
       align: "center",
-      render: (text, token) => (
-        <a>
-          {text} ({token.ticker})
-        </a>
-      ),
+      render: (_, token) => <span>{token.name}</span>,
     },
     {
       title: "Price",
-      dataIndex: "price",
+      dataIndex: "address",
       key: "price",
       width: "100px",
       align: "center",
-      render: (text) => <span>${text}</span>,
+      render: (address) => {
+        const tokenPrice = tokenPrices[`arbitrum:${address}`];
+        return tokenPrice ? <span>${tokenPrice}</span> : null;
+      },
     },
     {
-      title: "Holding",
-      dataIndex: "holding",
-      key: "holding",
+      title: "Balance",
+      dataIndex: "balance",
+      key: "balance",
       width: "100px",
       align: "center",
-      render: (text) => <span>{text}</span>,
+      render: (balance) => <span>{balance}</span>,
     },
     {
       title: "Value",
       key: "value",
       width: "100px",
       align: "center",
-      render: (_, token) => (
-        <span>${(token.price * token.holding).toFixed(2)}</span>
-      ),
-    },
-    {
-      title: "Action",
-      key: "action",
-      width: "100px",
-      align: "center",
-      render: (_, token, currentIndex) => {
-        return (
-          <Space size="middle">
-            <Button
-              className="Button"
-              type="primary"
-              onClick={() => handleEdit(currentIndex)}
-            >
-              Edit
-            </Button>
-            <Button
-              className="Button"
-              danger
-              onClick={() => handleDelete(currentIndex)}
-            >
-              Delete
-            </Button>
-          </Space>
-        );
+      render: (_, token) => {
+        const tokenPrice = tokenPrices[`arbitrum:${token.address}`];
+        const value = tokenPrice ? (tokenPrice * token.balance).toFixed(3) : null;
+        return value ? <span>${value}</span> : null;
       },
     },
   ];
-
-  const handleEdit = (currentIndex) => {
-    const token = tableData[currentIndex];
-    setEditedHolding(token.holding);
-    form.setFieldsValue({ holding: token.holding });
-    setModalVisible(true);
-    setEditIndex(currentIndex);
-  };
-
-  const handleDelete = (currentIndex) => {
-    const updatedTableData = [...tableData];
-    updatedTableData.splice(currentIndex, 1);
-    setTableData(updatedTableData);
-  };
-
-  const handleModalOk = () => {
-    form.validateFields().then((values) => {
-      const updatedTableData = [...tableData];
-      updatedTableData[editIndex].holding = values.holding;
-      setTableData(updatedTableData);
-      setModalVisible(false);
-      setEditedHolding(""); // Reset editedHolding state
-    });
-  };
-  
-
-  const handleModalCancel = () => {
-    setModalVisible(false);
-  };
-
-  const handleAddModalOk = () => {
-    form.validateFields().then(async (values) => {
-      const selectedToken = data.find((coin) => coin.address === addFormValues.token);
-  
-      // Check if the token already exists in the table
-      const tokenExists = tableData.some((token) => token.address === selectedToken.address);
-  
-      if (tokenExists) {
-        // Display a message or perform an action to indicate that the token already exists
-        message.error('Token already exists in the table');
-                return;
-      }
-  
-      const price = await fetchTokenPrice(`arbitrum:${selectedToken.address}`);
-      const newToken = {
-        address: addFormValues.token,
-        holding: values.holding,
-        img: selectedToken.img,
-        name: selectedToken.name,
-        ticker: selectedToken.ticker,
-        price: price || 1,
-      };
-      const tokenData = [...tableData, newToken];
-      setTableData(tokenData);
-      setAddModalVisible(false);
-      form.resetFields();
-    });
-  };
-  
-
-  const handleAddModalCancel = () => {
-    setAddModalVisible(false);
-    form.resetFields();
-  };
-
-  const handleTokenSelect = (value) => {
-    const selectedToken = data.find((coin) => coin.address === value);
-    setAddFormValues((prevValues) => ({
-      ...prevValues,
-      token: value,
-      price: selectedToken.price || 1,
-      img: selectedToken.img,
-      name: selectedToken.name,
-      ticker: selectedToken.ticker,
-    }));
-  };
-
-  const handleAddFormChange = (field, value) => {
-    setAddFormValues((prevValues) => ({
-      ...prevValues,
-      [field]: value,
-    }));
-  };
 
   const mainTable = () => (
     <>
       <Table
         className="portfolio-table-container && portfolio-table"
         columns={columns}
-        dataSource={tableData}
+        dataSource={balances}
         pagination={false}
-        
-        
-        
-        
       />
-      <Modal
-        visible={modalVisible}
-        onOk={handleModalOk}
-        onCancel={handleModalCancel}
-      >
-        <Form form={form}>
-          <Form.Item
-            name="holding"
-            label="Holding"
-            rules={[
-              { required: true, message: "Please enter the holding value" },
-            ]}
-          >
-            <Input
-              type="number"
-              placeholder="Enter the holding value"
-              value={editedHolding}
-              onChange={(e) => setEditedHolding(e.target.value)}
-            />
-          </Form.Item>
-        </Form>
-      </Modal>
     </>
   );
 
@@ -247,54 +141,10 @@ function Portfolio() {
     <div className="portfolio-container">
       <h2 className="tradeBoxHeader">
         Portfolio Watchlist
-        {isConnected && (
-          <div className="Button" onClick={() => setAddModalVisible(true)}>
-            Add
-          </div>
-        )}
+        {isConnected}
       </h2>
       {!isConnected && <p>Please connect your wallet</p>}
       {isConnected && mainTable()}
-      <Modal
-        visible={addModalVisible}
-        onOk={handleAddModalOk}
-        onCancel={handleAddModalCancel}
-      >
-        <Form form={form}>
-          <Form.Item
-            name="token"
-            label="Token"
-            rules={[
-              { required: true, message: "Please select a token" },
-            ]}
-          >
-            <Select
-              placeholder="Select a token"
-              onChange={(value) => handleTokenSelect(value)}
-            >
-              {data.map((coin) => (
-                <Option key={coin.address} value={coin.address}>
-                  {coin.name}
-                </Option>
-              ))}
-            </Select>
-          </Form.Item>
-          <Form.Item
-            name="holding"
-            label="Holding"
-            rules={[
-              { required: true, message: "Please enter the holding value" },
-            ]}
-          >
-            <Input
-              type="number"
-              placeholder="Enter the holding value"
-              value={addFormValues.holding}
-              onChange={(e) => handleAddFormChange("holding", e.target.value)}
-            />
-          </Form.Item>
-        </Form>
-      </Modal>
     </div>
   );
 }
